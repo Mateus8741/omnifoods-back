1e2577c070a5d9461538a34bbd4842a826463ea1
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-21 12:47:30 -0300

    Update listProducts route to return product data

--- src/routes/listProducts.ts
+++ src/routes/listProducts.ts
@@ -1,7 +1,13 @@
 import { FastifyInstance } from "fastify";
+import { prisma } from "../prisma/prisma-client";
 
 export async function listProducts(app:FastifyInstance) {
+    const data = prisma.product.findMany({
+        include: { details: true }
+    });
+
     app.get("/product", async (request, reply) => {
-        return { hello: "world" };
+
+        return data;
     });
 }
