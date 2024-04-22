5d209e8f637f81e3ac0c0caf3bedbeb765b85cd1
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-22 15:44:18 -0300

    Add @fastify/cors, @fastify/swagger, and @fastify/swagger-ui dependencies, and register them in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import fastify, { FastifyInstance } from "fastify";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastify from "fastify";
+import { ZodTypeProvider, jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createOrder } from "./routes/Orders/createOrder";
 import { listOrder } from "./routes/Orders/listOrder";
 import { createProduct } from "./routes/Products/createProduct";
@@ -7,7 +7,33 @@ import { listProducts } from "./routes/Products/listProducts";
 import { updateProductDetails } from "./routes/Products/updateProductDetails";
 import { updateTitle } from "./routes/Products/updateTitle";
 
-const app: FastifyInstance = fastify();
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
+import fastifyCors from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: "*",
+});
+
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ["application/json"],
+        produces: ["application/json"],
+        info: {
+            title: "OmniFoods API",
+            description: "Rotas do OmniFoods",
+            version: "1.0.0",
+        },
+    },
+    transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: "/docs",
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -22,4 +48,5 @@ app.register(listOrder);
 
 app.listen({
     port: 3100,
+    host: "0.0.0.0",
 }, () => console.log('Server is running on port 3100'));
