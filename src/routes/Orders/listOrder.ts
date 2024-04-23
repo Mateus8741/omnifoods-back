f0b1ed53da03aeae8b37aa03be3944cb44d06762
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-23 17:19:34 -0300

    Update listOrder route to include orderBy createdAt in listOrder.ts

--- src/routes/Orders/listOrder.ts
+++ src/routes/Orders/listOrder.ts
@@ -4,7 +4,8 @@ import { prisma } from "../../prisma/prisma-client";
 export async function listOrder(app: FastifyInstance) {
     app.get("/order", async (request, reply) => {
         const data = await prisma.order.findMany({
-            include: { productOrders: true }
+            include: { productOrders: true },
+            orderBy: { createdAt: "desc" }
         });
 
         return data
