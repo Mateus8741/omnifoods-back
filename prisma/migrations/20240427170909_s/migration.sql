2d4509ccfbb738038116e292d454142c780c28ee
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-04-27 14:09:18 -0300

    Add default value for changeToOrder field in ProductOrders model

---
+++ prisma/migrations/20240427170909_s/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_ProductOrders" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "productName" TEXT NOT NULL,
+    "productPrice" REAL NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "changeToOrder" TEXT NOT NULL DEFAULT '',
+    "orderId" TEXT NOT NULL,
+    CONSTRAINT "ProductOrders_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_ProductOrders" ("changeToOrder", "id", "orderId", "productName", "productPrice", "quantity") SELECT "changeToOrder", "id", "orderId", "productName", "productPrice", "quantity" FROM "ProductOrders";
+DROP TABLE "ProductOrders";
+ALTER TABLE "new_ProductOrders" RENAME TO "ProductOrders";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
