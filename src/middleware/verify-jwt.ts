064b42727286e9e6ddc7d05210f1c0e0e29ce7cd
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-06-22 13:49:32 -0300

    feat: Add getCurrentUserId method to FastifyRequest interface

---
+++ src/middleware/verify-jwt.ts
@@ -0,0 +1,28 @@
+// import { FastifyReply, FastifyRequest } from 'fastify'
+
+import { FastifyInstance } from "fastify"
+import fastifyPlugin from "fastify-plugin"
+
+// export async function verifyJwt(request: FastifyRequest, reply: FastifyReply) {
+//   try {
+//     await request.jwtVerify()
+//   } catch (err) {
+//     return reply.status(401).send({ message: 'Unauthorized.' })
+//   }
+// }
+
+
+
+export const auth = fastifyPlugin(async (app: FastifyInstance) => {
+  app.addHook('preHandler', async (request) => {
+    request.getCurrentUserId = async () => {
+      try {
+        const { sub } = await request.jwtVerify<{ sub: string }>()
+
+        return sub
+      } catch {
+        throw new Error('Unauthorized')
+      }
+    }
+  })
+})
